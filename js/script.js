cb5832c14beaf4e79ad7d9c97afb0cb05ec8c090
jimmymess86 <jimmymess86@gmail.com>
2021-09-03 21:03:04 -0400

    added pokemonList to js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+  {name: "Bulbasur", type: "grass", height: 0.7},
+  {name: "Pikachu", type: ["field", "electric"], height: 0.4},
+  {name: "Charizard", type: ["monster", "dragon"], height: 1.7,}
+];
